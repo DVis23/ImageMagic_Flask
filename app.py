dd3b7d275d7ad77cdee5624313de01dc4680e46c
DVisTaskOne <daniilv23@mail.ru>
2023-04-28 17:12:09 +0300

    initial window design

--- app.py
+++ app.py
@@ -22,6 +22,22 @@ def index():
 @app.route('/edit/<filename>', methods=['GET', 'POST'])
 def edit(filename):
     photo = photos.url(filename)
+    if request.method == 'POST':
+        img = Image.open(photos.path(filename))
+        bright_value = request.form.get('brightness')
+        contrast_value = request.form.get('contrast')
+        saturation_value = request.form.get('saturation')
+        if bright_value:
+            enhancer = ImageEnhance.Brightness(img)
+            img = enhancer.enhance(float(bright_value))
+        if contrast_value:
+            enhancer = ImageEnhance.Contrast(img)
+            img = enhancer.enhance(float(contrast_value))
+        if saturation_value:
+            enhancer = ImageEnhance.Color(img)
+            img = enhancer.enhance(float(saturation_value))
+        img.save(photos.path(filename))
+        return redirect(url_for('edit', filename=filename))
     return render_template('edit.html', photo=photo, filename=filename)
 
 
